93fc04bc7fa86a580641b6b68a3d94b109996430
zBiggui <bigguisenna@gmail.com>
2022-05-31 19:11:31 -0300

    Testing: Chai & Sinon

--- test/math.spec.js
+++ test/math.spec.js
@@ -1,6 +1,12 @@
 // mocha
 const assert = require('assert');
-const Math = require('../11_math.js');
+const Math = require('../11_mocha_math.js');
+
+// chai
+const expect = require('chai').expect;
+
+// Sinon
+const sinon = require('sinon');
 
 let value = 0;
 
@@ -29,16 +35,51 @@ describe('Math class', function () {
         // }
 
         math.sum(value, 5, (value) => {
-            assert.equal(value, 10);
+            // assert.equal(value, 10);
+
+            expect(value).to.equal(10);
 
             // For async methods
             done();
         });
     });
 
-    it.only('Multiply two numbers', function () {
+    it('Multiply two numbers', function () {
         const math = new Math();
 
-        assert.equal(math.multiply(value, 5), 25);
+        const obj = {
+            name: 'Guilherme Senna'
+        };
+
+        const obj2 = {
+            name: 'Guilherme Senna'
+        };
+
+        // assert.equal(math.multiply(value, 5), 25);
+
+        // expect(math.multiply(value, 5)).to.equal(0);
+
+        // expect(obj).to.have.property('name')
+        // expect(obj).to.have.property('name').equal('Guilherme')
+        // expect(obj).to.equal(obj2);
+        expect(obj).to.deep.equal(obj2);
+    });
+
+    it.only('Calls req with sum and index values', function () {
+        const req = {};
+        const res = {
+            load: function load() {
+                console.log('Called!');
+            }
+        };
+
+        sinon.spy(res, 'load')
+        // sinon.stub(res, 'load').returns('xpto');
+
+        const math = new Math();
+
+        math.printSum(req, res, 5, 5);
+
+        expect(res.load.args[0][1]).to.equal(10);
     });
 });
